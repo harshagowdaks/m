e6e9e0643fa810722eb369dad139b5d6057ca77f
MANJU SK <MANJU SK@DESKTOP-VPCK910>
2022-01-16 21:07:36 -0800

    commit_final

---
+++ Automation/src/Collection/hello1.java
@@ -0,0 +1,12 @@
+package Collection;
+
+public class hello1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mask = 0b101010101010;
+		short Short = (short)0b1010000101000101;
+		System.out.println(Short);
+	}
+
+}
